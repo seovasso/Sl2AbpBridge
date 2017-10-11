972c5569dc5d5ef91a2170ec3dd9c9b947eb0c32
seovasso <seo-vasso@yandex.ru>
2017-10-11 16:08:25 +0300

    1) Исправлен reset на reset_n 2) Убран вход en 3) Модуль переделан на двойной счетчик, убрана обратная связь 4) Ненужный case заменен на if 5) Добавлены комментарии TODO: 1) Разобраться с портами ready и en 2) Добавить делитель частоты

--- sources_1/new/SlTransmitter.sv
+++ sources_1/new/SlTransmitter.sv
@@ -26,25 +26,26 @@ module SlTransmitter(
     output logic ready,
     input  logic [31:0] data,
     input  logic [1:0] mode,
-    input  logic en,
     input  logic clk,
-    input  logic reset
+    input  logic reset_n
     );
     logic [31:0] buff;//буфер для данных
-    logic [5:0] counter;//переменная счетчика
-    logic [5:0] maxcount;//максимальное число, до которого считает счетчик
+    logic [6:0] counter;//переменная счетчика
+    logic [6:0] maxCount;//максимальное число, до которого считает счетчик
+    logic [5:0] endBit;//номер бита завершения, когда sl0=0 и sl1=0 
     logic parSl0;//четность sl0
     logic parSl1;//четность sl1
-    always_comb begin
+    always_comb begin:maxcount_get //нахождение максимального числа счетчика
         case (mode)
-            0: maxcount=6'd8 ;
-            1: maxcount=6'd16;
-            2: maxcount=6'd32;
-            default: maxcount=6'd0;//непредвиденная ситуация
+            0: maxCount = 7'd19;
+            1: maxCount = 7'd35;
+            2: maxCount = 7'd67;
+            default: maxCount=6'd0;//непредвиденная ситуация
         endcase
-    end
-    always_ff @(posedge clk, posedge reset) begin
-        if (reset) begin
+    end:maxcount_get
+    assign endBit=maxCount[6:1];
+    always_ff @(posedge clk, negedge reset_n) begin
+        if (!reset_n) begin
             counter<=6'b0;
             ready  <=1'b1;
             parSl0 <=1'b1;
@@ -52,43 +53,46 @@ module SlTransmitter(
             sl0    <=1'b1;
             sl1    <=1'b1;
         end else begin
-            if (en) begin
                 if (ready) begin
-                    buff<=data;
+                    buff<=data; 
                     ready<=1'b0;
-                end else begin
-                    if ((sl0==0)||(sl1==0)) begin
-                        sl0<=1;
-                        sl1<=1;
+                end else 
+                begin
+                    if (counter[0]) begin // на нечетных тактах возвращаем шину обратно 
+                        sl0 <= 1;
+                        sl1 <= 1;
+
                     end else begin
-                        if(counter<maxcount)begin
-                            case(buff[0])
-                                1'b0:begin 
-                                    sl0<=0;
-                                    parSl0<=~parSl0;
+                        if( counter[6:1] < (endBit-1))  begin //отправка битов сообщения
+                            if( !buff[0] )begin //в зависимости от содержимого буфера отправляем 0 или 1 
+                                    sl0 <= 0;
+                                    parSl0 <= ~parSl0; // подсчет четности по 0-м
+                                end else begin
+                                    sl1 <= 0;
+                                    parSl1 <= ~parSl1; // подсчет четности по 1-м
                                 end
-                                    1'b1: begin
-                                    sl1<=0;
-                                    parSl1<=~parSl1;
-                                end
-                            endcase
-                            for (int i=0; i <= 30; i=i+1) buff[i]<=buff[i+1];
-                            counter<=counter+1;
+                            buff <= buff >> 1; // сдвиг регистра
                         end else
-                        if(counter==maxcount)begin
-                            sl0<=parSl0;
-                            sl1<=parSl1;
-                            counter<=counter+1;
-                        end else begin
-                            sl0<=1'b0;
-                            sl1<=1'b0;
-                            counter<=6'd0;
-                            ready<=1'b1;
-                            parSl0 <=1'b1;
-                            parSl1 <=1'b0;
+                        if(counter[6:1] == (endBit-1))begin//отправка бита четности
+                            sl0 <= parSl0; // бит четности по 0
+                            sl1 <= parSl1; // бит четности по 1 
+                        end else 
+                        if(counter[6:1] == (endBit)) begin //завершение сообщения 
+                            sl0 <= 1'b0; // выставляем на обоих каналах 0-и
+                            sl1 <= 1'b0; 
+                            ready <= 1'b1; // бит окончания отправки
+                            parSl0 <= 1'b1; //выставляем биты подсчета четности в исходное состояние
+                            parSl1 <= 1'b0;
+                        end else
+                        if(counter [6:1] == (endBit)) begin //сбой в работе счетчика
+                            sl0 <= 1'b1;
+                            sl1 <= 1'b1;
+                            ready <= 1'b1;
+                            parSl0 <= 1'b1;
+                            parSl1 <= 1'b0;
                         end
-                    end                        
-                end
+                    end   
+                    counter <= (counter<maxCount?(counter+1):6'd0);//инкрементируем счетчик             
             end
         end
     end
