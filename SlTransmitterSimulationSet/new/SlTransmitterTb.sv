b67ad82af8bfb1d31e3f8f5db629dfc33b55eb80
seovasso <seo-vasso@yandex.ru>
2017-10-11 12:37:01 +0300

    Простенькое тестовое окружение для передатчика

---
+++ SlTransmitterSimulationSet/new/SlTransmitterTb.sv
@@ -0,0 +1,53 @@
+`timescale 100ps / 1ps
+`include "C:/Users/Vasso/Documents/vivadoprojects/project_1/project_1.srcs/sources_1/new/SlTransmitter.sv"
+
+module SlTransmitterTb();
+
+     logic reset; //ресет
+     logic sl0wire;//для соединения модулей
+     logic sl1wire;//для соединения модулей
+     logic [1:0] mode; // 
+     logic [31:0] dataIn;
+     logic [31:0] dataOut;
+     logic transReady;
+     logic recReady;
+     logic en; // включение передатчика
+     logic valid;// правильность сообщения, принятого приемником
+     logic clk; //тактовый сигнал
+
+    SlTransmitter trans (
+    .reset(reset),
+     .sl0(sl0wire),
+     .sl1(sl1wire),
+    .mode(mode),
+    .data(dataIn),
+     .en(en),
+    .ready(transReady),
+    .clk(clk)
+    );
+     SlReciever res (
+    .reset(reset),
+     .sl0(sl0wire),
+     .sl1(sl1wire),
+    .mode(mode),
+    .data(dataOut),
+     .valid(valid),
+    .ready(recReady)
+    );
+    initial begin
+    forever #5 clk<=~clk;
+    end
+    initial begin
+        clk=0;
+        mode=2'b00;
+        dataIn=32'd134;
+        en=1;
+        
+        reset = 0;
+        #10
+        reset = 1;
+        #10 
+        reset=0;
+        #100;
+    end
+endmodule
