ceba800f4820baac752ed866549b24962d0ccc5d
seovasso <seo-vasso@yandex.ru>
2017-10-13 14:14:27 +0300

    Описаны простейшие механизмы записи и чтения

--- rtl/Apb2Sl.sv
+++ rtl/Apb2Sl.sv
@@ -20,15 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 parameter ADDR_WIDTH = 10;//ширина шины адрес
-parameter DATA_REG_ADDR = ADDR_WIDTH'd5; //адрес регистра данных
-parameter CONFIG_REG_ADDR = ADDR_WIDTH'd6; //адрес конфигурационного регистра
-parameter STATUS_REG_ADDR = ADDR_WIDTH'd7; //адрес статусного регистра
+parameter DATA_REG_ADDR = 10'd5; //адрес регистра данных
+parameter CONFIG_REG_ADDR = 10'd6; //адрес конфигурационного регистра
+parameter STATUS_REG_ADDR = 10'd7; //адрес статусного регистра
+parameter CONFIG_REG_WIDTH = 8;//размер конфигурационного регистра
+parameter STATUS_REG_WIDTH = 8;//размер статусного регистра
 module Apb2Sl(
 input                      clk, //синхронизация обычная
 input                      reset_n, //обычный ресет
 input                      pclk, //синхронизация шины
 input                      preset_n, //ресет apb
-input   [ADDR_WIDTH-1:0]    paddr,
+input   [ADDR_WIDTH-1:0]   paddr,
 input                      psel1,
 input                      peneble,
 input                      pwrite,
@@ -39,5 +41,49 @@ output  logic [31:0]       prdata,
 output                     pslverr
 //TODO: Описать выходы
     );
+//внутренние переменные: 
+logic [CONFIG_REG_WIDTH-1:0] confApbReg;//буферные регистры синхронизирующиеся по клоку apb
+logic [STATUS_REG_WIDTH-1:0] statApbReg;
+logic [31:0]                 dataApbReg;
+logic                        transactionStarted;// переменная гласящая о начале транзакции 
+//переменные для генерации: 
+genvar i;
+ 
+  always_ff @(posedge pclk, negedge preset_n)
+  begin: apb_transaction
+    if (!preset_n) begin
+      confApbReg<=0;
+      statApbReg<=0;
+      dataApbReg<=0;
+    end else begin
+      if (psel1) begin
+        if((paddr == DATA_REG_ADDR) 
+            ||(paddr == CONFIG_REG_ADDR)
+             ||(paddr == STATUS_REG_ADDR))// проверка адреса
+         begin
+          if (1) pready<=1;//TODO: добавить проверку синхронизации
+         end
+         //описание задержки если 
+         // описание процессов записи и чтения
+         if (pready&&penable) begin     //если в предыдущем такте было выставлено pready
+          if(pwrite) begin//запись 
+            case(paddr)//пишем в регистры
+              DATA_REG_ADDR: for(i=0;i<=3;i++) if(pstrb[i]) dataApbReg[8*i+7:8*i]<=pwdata[8*i+7:8*i];
+              CONFIG_REG_ADDR:configApbReg<=pwdata[CONFIG_REG_WIDTH-1:0];
+              STATUS_REG_ADDR:statusApbReg<=pwdata[STATUS_REG_WIDTH-1:0];
+              default:PSLVERR<=1;
+            endcase
+          end else begin//чтение
+            unique case(paddr)//читаем из регистров
+              DATA_REG_ADDR: pwdata<=dataApbReg;
+              CONFIG_REG_ADDR:pwdata[CONFIG_REG_WIDTH-1:0]<=configReg;
+              STATUS_REG_ADDR:pwdata[STATUS_REG_WIDTH-1:0]<=statusReg;
+              default:PSLVERR<=1;
+            endcase
+          end
+         end
+        end
+      end      
 
+  end
 endmodule
