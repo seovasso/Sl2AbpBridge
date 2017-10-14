3c31aafbdabd24bafed753ea6df56377bc2b4c50
seovasso <seo-vasso@yandex.ru>
2017-10-14 14:48:11 +0300

    Результат пятничной работы. Приблизительно описана логика модуля. TODO: Закончить отладку и запустить уже хоть что-то.

--- bench/hdl/ApbSlTb.sv
+++ bench/hdl/ApbSlTb.sv
@@ -18,49 +18,68 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
+parameter clkPeriod=10;//период клока
+parameter clkTimeDiff=3;//смещение тактовых сигналов относительно друг друга
+parameter paddrWidth=10;// ширина адресной шины apb
 module Apb2SlTb(
 
     );
-    parameter clkPeriod=10;//период клока
-    parameter clkTimeDiff=3;//смещение тактовых сигналов относительно друг друга
-    parameter paddrWidth=10;// ширина адресной шины apb
+
     logic clk; //тактовые сигналы: блока
     logic pclk;// apb
     // сигналы шины apb
-    logic preset_n;
-    logic [paddrWidth-1:0] paddr;
-    logic pprot;
-    logic psel2;
-    logic penable;
-    logic pwrite;
-    logic [31:0] pwdata;
-    logic [31:0] prdata;
-    logic [3:0] pstrb;
-    logic pready;
-    logic pdata;
-    logic pslverr;
+    logic                   preset_n;
+    logic                   reset_n;
+    logic [paddrWidth-1:0]  paddr;
+    logic                   pprot;
+    logic                   psel2;
+    logic                   penable;
+    logic                   pwrite;
+    logic [31:0]            pwdata;
+    logic [31:0]            prdata;
+    logic [3:0]             pstrb;
+    logic                   pready;
+    logic                   pdata;
+    logic                   pslverr;
+    //определение модуля
+         Apb2Sl mod (
+          .clk(clk),
+          .pclk(pclk),
+          .reset_n(reset_n),
+          .psel1(psel2),
+          .paddr(paddr),
+          .pwdata(pwdata),
+          .prdata(pwdata),
+          .penable(penable),
+          .pready(pready),
+          .pslverr(pslverr),
+          .pstrb(pstrb),
+          .preset_n(preset_n)
+   );
+    
+    
     // сценарии транзакций чтения и записи
     task writeTransaction;
       input bit [31:0] wrData;
       input bit [paddrWidth-1:0] wrAddr;
       begin 
       #clkPeriod;
-      paddr=wrAddr;
-      pwrite=1;
-      penable=0;
-      pwdata=wrData;
-      psel2=1;
+        paddr=wrAddr;
+        pwrite=1;
+        penable=0;
+        pstrb=4'b1111;
+        pwdata=wrData;
+        psel2=1;
       #clkPeriod;
-      penable=1;
+        penable=1;
       #clkPeriod;
       //while(!pready)begin
-        #clkPeriod;
-      psel2=0;
-      penable=0;
-      pwdata=0;
-      paddr=0;   
-      pwrite=0; 
+        psel2=0;
+        penable=0;
+        pwdata=0;
+        paddr=0; 
+        pstrb=0;  
+        pwrite=0; 
       end
     endtask;
     task readTransaction();
@@ -79,9 +98,21 @@ module Apb2SlTb(
         forever #(clkPeriod/2) pclk<=~pclk;//второй клок
       end
      initial begin
+     //инициализация
      clk=0;
      pclk=0;
-     
+     preset_n=1;
+     paddr=0;
+     pprot=0;
+     psel2=0;
+     penable=0;
+     pwrite=0;
+     pwdata=0;
+     pstrb=0;
+     #25;
+     preset_n=0;
+     #15;
+     preset_n=1;
      #40
      writeTransaction(10'd2,32'd3156);
      end
--- rtl/Apb2Sl.sv
+++ rtl/Apb2Sl.sv
@@ -32,28 +32,28 @@ input                      pclk, //синхронизация шины
 input                      preset_n, //ресет apb
 input   [ADDR_WIDTH-1:0]   paddr,
 input                      psel1,
-input                      peneble,
+input                      penable,
 input                      pwrite,
 input   [31:0]             pwdata,
 input   [3:0]              pstrb,
 output  logic              pready,
 output  logic [31:0]       prdata,
-output                     pslverr
+output  logic              pslverr
 //TODO: Описать выходы
     );
 //внутренние переменные: 
-logic [CONFIG_REG_WIDTH-1:0] confApbReg;//буферные регистры синхронизирующиеся по клоку apb
-logic [STATUS_REG_WIDTH-1:0] statApbReg;
+logic [CONFIG_REG_WIDTH-1:0] configApbReg;//буферные регистры синхронизирующиеся по клоку apb
+logic [STATUS_REG_WIDTH-1:0] statusApbReg;
 logic [31:0]                 dataApbReg;
 logic                        transactionStarted;// переменная гласящая о начале транзакции 
 //переменные для генерации: 
 genvar i;
- 
+
   always_ff @(posedge pclk, negedge preset_n)
   begin: apb_transaction
     if (!preset_n) begin
-      confApbReg<=0;
-      statApbReg<=0;
+      configApbReg<=0;
+      statusApbReg<=0;
       dataApbReg<=0;
     end else begin
       if (psel1) begin
@@ -61,29 +61,41 @@ genvar i;
             ||(paddr == CONFIG_REG_ADDR)
              ||(paddr == STATUS_REG_ADDR))// проверка адреса
          begin
-          if (1) pready<=1;//TODO: добавить проверку синхронизации
+          transactionStarted<=1;
+          if (1) pready<=1;//TODO: добавить проверку на начало транзиакции
          end
          //описание задержки если 
          // описание процессов записи и чтения
          if (pready&&penable) begin     //если в предыдущем такте было выставлено pready
           if(pwrite) begin//запись 
-            case(paddr)//пишем в регистры
-              DATA_REG_ADDR: for(i=0;i<=3;i++) if(pstrb[i]) dataApbReg[8*i+7:8*i]<=pwdata[8*i+7:8*i];
+            unique case(paddr)//пишем в регистры
+              DATA_REG_ADDR: begin
+              if(pstrb[0]) dataApbReg[7:0]<=pwdata[7:0];
+              if(pstrb[1]) dataApbReg[15:8]<=pwdata[15:8];
+              if(pstrb[2]) dataApbReg[23:16]<=pwdata[23:16];
+              if(pstrb[3]) dataApbReg[31:24]<=pwdata[31:24];
+//               generate
+//                for( i = 0; i <= 3; i = i + 1) if(pstrb[i]) dataApbReg[(8*i+7):(8*i)]<=pwdata[(8*i+7):(8*i)];
+//               endgenerate
+//                почему это не работает????
+                end
               CONFIG_REG_ADDR:configApbReg<=pwdata[CONFIG_REG_WIDTH-1:0];
               STATUS_REG_ADDR:statusApbReg<=pwdata[STATUS_REG_WIDTH-1:0];
-              default:PSLVERR<=1;
+              default:pslverr<=1;
             endcase
+            pready<=0;
+            transactionStarted<=0;
           end else begin//чтение
             unique case(paddr)//читаем из регистров
-              DATA_REG_ADDR: pwdata<=dataApbReg;
-              CONFIG_REG_ADDR:pwdata[CONFIG_REG_WIDTH-1:0]<=configReg;
-              STATUS_REG_ADDR:pwdata[STATUS_REG_WIDTH-1:0]<=statusReg;
-              default:PSLVERR<=1;
+              DATA_REG_ADDR: prdata<=dataApbReg;
+              CONFIG_REG_ADDR:prdata[CONFIG_REG_WIDTH-1:0]<=configApbReg;
+              STATUS_REG_ADDR:prdata[STATUS_REG_WIDTH-1:0]<=statusApbReg;
+              default:pslverr<=1;
             endcase
-          end
+          end 
+          if (!pready) prdata<=0; 
          end
         end
       end      
-
   end
 endmodule
